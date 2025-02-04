30beb9cf262535f5fd80bb6a5f9e2f7645a0c0fa
Taksanzu <ankhang897979@gmail.com>
2025-02-04 14:52:15 +0700

    add player boost speed

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,22 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D playerRigidbody;
-    [SerializeField] private float torqueForce = 3f;
+
+    [SerializeField] float torqueForce = 3f;
+    [SerializeField] float bootsForce = 20f;
+    [SerializeField] float baseSpeed = 10f;
+
+    Rigidbody2D playerRigidbody;
+    SurfaceEffector2D surfaceEffector2D;
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+        surfaceEffector2D.speed = baseSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)){
-            playerRigidbody.AddTorque(torqueForce);  
-        } else if (Input.GetKey(KeyCode.RightArrow)){
+        RotatePlayer();
+        BoostSpeed();
+    }
+
+    private void BoostSpeed()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            surfaceEffector2D.speed = bootsForce;
+        }else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerRigidbody.AddTorque(torqueForce);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
             playerRigidbody.AddTorque(-torqueForce);
         }
     }
